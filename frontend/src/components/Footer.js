7bffbd576618a4aa58c8e5691989620345383d27
pooja <senbhavin@gmail.com>
2021-09-22 10:30:47 +0530

    added changes

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,7 @@
+import React from "react";
+import "./Footer.css";
+const Footer = () => {
+  return <div id="footer"></div>;
+};
+
+export default Footer;
---
+++ frontend/src/screens/StudentAdmitCard.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Link } from "react-router-dom";
+const StudentAdmitCard = () => {
+  return (
+    <div className="container1">
+      <div className="admitCard-outer">
+        <Link className="link" to="/admit_card/allstudents">
+          Print admit card of all students
+        </Link>
+        <Link className="link" to="/admit_card/classes">
+          Print admit card of particular class
+        </Link>
+        <Link className="link" to="/admit_card/student">
+          Print admit card of particular student
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default StudentAdmitCard;
